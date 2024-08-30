f3d170b9bb28daf840549bbe4a7febbf7404e588
Eduwin Fabian Torres Mendoza <127137254+Eduwinnerjoin@users.noreply.github.com>
2024-08-30 14:40:34 +0000

    Crear archivo string

---
+++ ejercicios.rb
@@ -0,0 +1,8 @@
+texto = "hola mundo"
+puts texto.upcase  # Salida: "HOLA MUNDO"
+
+texto = "HOLA MUNDO"
+puts texto.downcase  # Salida: "hola mundo"
+
+texto = "hola mundo, hola Ruby"
+puts texto.gsub("hola", "hello")  # Salida: "hello mundo, hello Ruby"
